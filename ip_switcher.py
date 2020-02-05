5e7b8934c8ef040fa7d574981484a9320bd3c683
itay121 <43811398+itay121@users.noreply.github.com>
2020-02-05 15:26:05 +0200

    Add files via upload

---
+++ ip_switcher.py
@@ -0,0 +1,126 @@
+"""
+from lxml.html import fromstring
+import requests
+from itertools import cycle
+import traceback
+
+
+def get_proxies():
+    url = 'https://free-proxy-list.net/'
+    response = requests.get(url)
+    parser = fromstring(response.text)
+    proxies = set()
+    for i in parser.xpath('//tbody/tr')[:10]:
+        if i.xpath('.//td[7][contains(text(),"yes")]'):
+            proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
+            proxies.add(proxy)
+    return proxies
+
+
+# If you are copy pasting proxy ips, put in the list below
+# proxies = ['121.129.127.209:80', '124.41.215.238:45169', '185.93.3.123:8080', '194.182.64.67:3128', '106.0.38.174:8080', '163.172.175.210:3128', '13.92.196.150:8080']
+my_data = {}
+j = 0
+counter = 0
+start = 0
+data_base = open("ip_list.txt", "r").read()
+# print(data_base)
+# for o in data_base:
+need = True
+for i in data_base:
+    if i == "\t" or i == "	" or i == "\n" or i == r"\n" or i == "_":
+        start = counter
+        j = 0
+    if i == ".":
+        j += 1
+        if j == 3:
+            if need:
+                need = False
+                end = data_base.find("_", start)
+                port = data_base[end - 1: data_base.find("_", end + 1)]
+                # print(port)
+                my_data[data_base[start + 1: end]] = port[2:]
+                j = 0
+            else:
+                need = True
+    counter += 1
+print(my_data)
+# print(data_base)
+proxies = []
+for i in my_data:
+    p = ""
+    for j in i:
+        if j.isdigit() or j == ".":
+            p = p + j
+    proxies.append(p + ":" + my_data[i])
+print(proxies)
+# proxies = get_proxies()
+proxy_pool = cycle(proxies)
+
+url = 'https://httpbin.org/ip'
+for i in range(1,100):
+    # Get a proxy from the pool
+    proxy = next(proxy_pool)
+    print("Request #%d" % i)
+    try:
+        response = requests.get(url, proxies={"http": proxy, "https": proxy})
+        print(response.json())
+    except:
+        # Most free proxies will often get connection errors. You will have retry the entire request using another proxy to work.
+        # We will just skip retries as its beyond the scope of this tutorial and we are only downloading a single url
+        print("Skipping. Connnection error")
+        """
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+import random
+
+ua = UserAgent() # From here we generate a random user agent
+proxies = [] # Will contain proxies [ip, port]
+
+# Main function
+def main():
+  # Retrieve latest proxies
+  proxies_req = Request('https://www.sslproxies.org/')
+  proxies_req.add_header('User-Agent', ua.random)
+  proxies_doc = urlopen(proxies_req).read().decode('utf8')
+
+  soup = BeautifulSoup(proxies_doc, 'html.parser')
+  proxies_table = soup.find(id='proxylisttable')
+
+  # Save proxies in the array
+  for row in proxies_table.tbody.find_all('tr'):
+    proxies.append({
+      'ip':   row.find_all('td')[0].string,
+      'port': row.find_all('td')[1].string
+    })
+
+  # Choose a random proxy
+  proxy_index = random_proxy()
+  proxy = proxies[proxy_index]
+
+  for n in range(1, 100):
+    req = Request('http://icanhazip.com')
+    req.set_proxy(proxy['ip'] + ':' + proxy['port'], 'http')
+
+    # Every 10 requests, generate a new proxy
+    if n % 10 == 0:
+      proxy_index = random_proxy()
+      proxy = proxies[proxy_index]
+
+    # Make the call
+    try:
+      my_ip = urlopen(req).read().decode('utf8')
+      print('#' + str(n) + ': ' + my_ip)
+    except: # If error, delete this proxy and find another one
+      del proxies[proxy_index]
+      print('Proxy ' + proxy['ip'] + ':' + proxy['port'] + ' deleted.')
+      proxy_index = random_proxy()
+      proxy = proxies[proxy_index]
+
+# Retrieve a random index proxy (we need the index to delete it if not working)
+def random_proxy():
+  return random.randint(0, len(proxies) - 1)
+
+if __name__ == '__main__':
+  main()
